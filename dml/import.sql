7fe4235baa1322bbd9cc8ff4f7b24645426ef681
Peter Yates <peter.yates@graphia.co.uk>
2020-03-29 14:22:45 +0100

    Add open, close, censure date and pupil counts

--- dml/import.sql
+++ dml/import.sql
@@ -3,7 +3,13 @@ insert into schools (
 	name,
 	establishment_type,
 	establishment_type_group,
-	open
+	open,
+	opened_on,
+	closed_on,
+	censused_on,
+	pupils,
+	boys,
+	girls
 )
 
 select
@@ -12,12 +18,37 @@ select
 	sr."TypeOfEstablishment (name)"::establishment,
 	sr."EstablishmentTypeGroup (name)"::establishment_group,
 
-	case
+	case -- open
 	when (sr."EstablishmentStatus (name)" = 'Open' or sr."EstablishmentStatus (name)" = 'Open, but proposed to close')
 		then true
 	else -- "Proposed to open" or "Closed"
 		false
-	end
+	end,
+
+	case -- opened_on
+	when (sr."OpenDate" is null or sr."OpenDate" = '')
+		then null
+	else
+		sr."OpenDate"::date
+	end,
+
+	case -- closed_on
+	when (sr."CloseDate" is null or sr."CloseDate" = '')
+		then null
+	else
+		sr."CloseDate"::date
+	end,
+
+	case -- censused_on
+	when (sr."CensusDate" is null or sr."CensusDate" = '')
+		then null
+	else
+		sr."CensusDate"::date
+	end,
+
+	nullif(sr."NumberOfPupils", '')::integer,
+	nullif(sr."NumberOfBoys", '')::integer,
+	nullif(sr."NumberOfGirls", '')::integer
 
 from
 	schools_raw sr;
