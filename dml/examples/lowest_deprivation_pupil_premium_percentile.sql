66c230bd67fbd997898bbed80428d887b11df948
Peter Yates <peter.yates@graphia.co.uk>
2020-04-28 13:53:52 +0100

    Add example queries from the README

---
+++ dml/examples/lowest_deprivation_pupil_premium_percentile.sql
@@ -0,0 +1,21 @@
+select
+    os.local_authority,
+    percentile_disc(0.4) within group (order by dpp.allocation) as "P40", -- discrete percentile at 0.4 (40%)
+    percentile_disc(0.5) within group (order by dpp.allocation) as "P50",
+    percentile_disc(0.6) within group (order by dpp.allocation) as "P60",
+    percentile_disc(0.7) within group (order by dpp.allocation) as "P70",
+    percentile_disc(0.8) within group (order by dpp.allocation) as "P80",
+    percentile_disc(0.9) within group (order by dpp.allocation) as "P90"
+from
+    deprivation_pupil_premium dpp
+inner join
+    open_schools os on dpp.urn = os.urn
+group by
+    local_authority
+having
+    count(*) > 15                                                         -- only select local authorities with more than fifteen schools
+order by
+    avg(dpp.allocation::decimal) asc                                      -- order by DPP allocation ascending, we want the lowest
+limit
+    20
+;
---
+++ dml/examples/within_3km_of_stonehenge.sql
@@ -0,0 +1,13 @@
+select
+  urn,
+  name
+from
+  open_schools
+where st_dwithin(
+  coordinates,                            -- Database column that holds the school's location
+  st_setsrid(
+    st_makepoint(-1.826194, 51.178868),   -- Stonehenge's coords
+    4326                                  -- World Geodetic System
+  ),
+  3000                                    -- Search radius in metres
+);
