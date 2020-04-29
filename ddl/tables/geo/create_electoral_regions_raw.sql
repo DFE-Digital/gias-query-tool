f00d8e65d43d0a52a1b915c46074775ca0d227b3
Peter Yates <peter.yates@graphia.co.uk>
2020-04-29 13:56:19 +0100

    Standardise on tabs for indentation
    Spaces originated from automatically-generated files from CSVKit and
    other places
    
    https://csvkit.readthedocs.io

--- ddl/tables/geo/create_electoral_regions_raw.sql
+++ ddl/tables/geo/create_electoral_regions_raw.sql
@@ -1,7 +1,7 @@
 create table electoral_regions_raw (
-    ogc_fid integer not null,
-    eer13cd character varying,
-    eer13cdo character varying,
-    eer13nm character varying,
-    wkb_geometry geometry(Geometry, 4326)
+	ogc_fid integer not null,
+	eer13cd character varying,
+	eer13cdo character varying,
+	eer13nm character varying,
+	wkb_geometry geometry(Geometry, 4326)
 );
--- dml/examples/within_3km_of_stonehenge.sql
+++ dml/examples/within_3km_of_stonehenge.sql
@@ -1,13 +1,13 @@
 select
-  urn,
-  name
+	urn,
+	name
 from
-  open_schools
+	open_schools
 where st_dwithin(
-  coordinates,                            -- Database column that holds the school's location
-  st_setsrid(
-    st_makepoint(-1.826194, 51.178868),   -- Stonehenge's coords
-    4326                                  -- World Geodetic System
-  ),
-  3000                                    -- Search radius in metres
+	coordinates,                            -- Database column that holds the school's location
+	st_setsrid(
+		st_makepoint(-1.826194, 51.178868), -- Stonehenge's coords
+		4326                                -- World Geodetic System
+	),
+	3000                                    -- Search radius in metres
 );
