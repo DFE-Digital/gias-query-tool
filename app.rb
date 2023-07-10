cf77e586903f8e9061aecc3d05748f0ec322f1c1
Duncan Brown <duncanjbrown@gmail.com>
2023-07-10 15:13:49 +0100

    Start API development

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'active_support'
+require 'sinatra/activerecord'
+
+class School < ActiveRecord::Base
+  def as_json
+    attrs = super.except("urn", "coordinates")
+    { id: urn, type: "school", attributes: attrs }
+  end
+end
+
+before '/api/*' do
+  content_type 'application/json'
+end
+
+get '/api/schools' do
+  School.first(10).as_json.to_json
+end
