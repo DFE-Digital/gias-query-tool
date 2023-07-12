6b94db40ff65fe2bf419045b8f1bfbe9c8869947
Duncan Brown <duncanjbrown@gmail.com>
2023-07-12 16:27:52 +0100

    Add /api/open_schools

---
+++ spec/requests/get_open_schools_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'json'
+
+RSpec.describe 'GET /open_schools' do
+  it 'returns a list of open schools' do
+    get '/api/open_schools'
+    expect(last_response.status).to eq(200)
+
+    resp = JSON.parse(last_response.body)
+    expect(resp['data'].length).to eq(81)
+
+    expect(resp).to be_valid_against_openapi_schema('SchoolList')
+  end
+end
