17506711e5d79b35265931e56730fa42c1568d2a
Peter Yates <peter.yates@graphia.co.uk>
2023-09-02 20:52:17 +0100

    Make the open schools request spec dynamic
    This test started failing because the number of open schools seems to
    have changed from 81 to 80. Here, it's not the number that's important
    but that the number matches the quantity in the test set.

--- spec/requests/get_open_schools_spec.rb
+++ spec/requests/get_open_schools_spec.rb
@@ -1,16 +1,36 @@
 require 'spec_helper'
 require 'json'
+require 'csv'
 
 RSpec.describe 'GET /open_schools' do
-  it 'returns a list of open schools' do
-    get '/api/open_schools'
-    expect(last_response.status).to eq(200)
+  let(:open_school_count) do
+    CSV
+      .read(Dir.glob('tmp/*test-set*').pop, headers: true)
+      .map { |r| r['EstablishmentStatus (name)'] }
+      .count('Open')
+  end
 
-    resp = JSON.parse(last_response.body)
+  let(:resp) do
+    JSON.parse(last_response.body)
+  end
+
+  before { get '/api/open_schools' }
+
+  it('returns with success') do
+    expect(last_response.status).to eq(200)
+  end
+
+  it 'only returns open schools' do
     resp['data'].each do |item|
       expect(item['attributes']['open']).to be(true)
     end
+  end
 
+  it 'returns all open schools' do
+    expect(resp['data'].length).to eq(open_school_count)
+  end
+
+  it 'is valid against openapi schema' do
     expect(resp).to be_valid_against_openapi_schema('SchoolList')
   end
 end
